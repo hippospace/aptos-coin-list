b546e367afdba8de8402931936711f87b6043c59
andy <andy@hippo.space>
2022-09-28 15:50:03 +0800

    Add new coin

--- typescript/src/requestList.ts
+++ typescript/src/requestList.ts
@@ -2,6 +2,75 @@ import { RawCoinInfo } from "./list";
 
 export const REQUESTS: RawCoinInfo[] = [
   {
+    "name": "Aptos Coin",
+    "symbol": "APT",
+    "coingecko_id": "aptos",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/APT.webp",
+    "project_url": "https://aptoslabs.com/",
+    "token_type": {
+      "type": "0x01::aptos_coin::AptosCoin",
+      "account_address": "0x01",
+      "module_name": "aptos_coin",
+      "struct_name": "AptosCoin"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
+    "name": "Ditto Staked Aptos",
+    "symbol": "stAPT",
+    "coingecko_id": "",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/DittoStakeAptos.svg",
+    "project_url": "https://www.dittofinance.io/",
+    "token_type": {
+      "type": "0x4d87417a2fb3248887d820f7737d9c4aeeb9591c5de91d08f7f490550e733894::staked_coin::StakedAptos",
+      "account_address": "0x4d87417a2fb3248887d820f7737d9c4aeeb9591c5de91d08f7f490550e733894",
+      "module_name": "staked_coin",
+      "struct_name": "StakedAptos"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  //  tortuga
+  {
+    "name": "StakedAptosCoin",
+    "symbol": "tAPT",
+    "coingecko_id": "",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/TortugaStakedAptos.png",
+    "project_url": "https://tortuga.finance/",
+    "token_type": {
+      "type": "0x12d75d5bde2535789041cd380e832038da873a4ba86348ca891d374e1d0e15ab::staked_aptos_coin::StakedAptosCoin",
+      "account_address": "0x12d75d5bde2535789041cd380e832038da873a4ba86348ca891d374e1d0e15ab",
+      "module_name": "staked_aptos_coin",
+      "struct_name": "StakedAptosCoin"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
+    "name": "Argo USD",
+    "symbol": "USDA",
+    "coingecko_id": "",
+    "decimals": 8,
+    "logo_url": "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/USDA.svg",
+    "project_url": "https://argo.fi/",
+    "token_type": {
+      "type": "0x1000000f373eb95323f8f73af0e324427ca579541e3b70c0df15c493c72171aa::usda::USDA",
+      "account_address": "0x1000000f373eb95323f8f73af0e324427ca579541e3b70c0df15c493c72171aa",
+      "module_name": "usda",
+      "struct_name": "USDA"
+    },
+    "extensions": {
+      "data": []
+    }
+  },
+  {
     "name": "DAI",
     "symbol": "devDAI",
     "coingecko_id": "dai",
