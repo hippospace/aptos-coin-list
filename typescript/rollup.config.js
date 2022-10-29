7af9c8bab535bb559717363f26f5812dc5822516
andy <andy@hippo.space>
2022-10-29 13:46:58 +0800

    use rollup build

---
+++ typescript/rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json'
+var rollup_config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: './dist/index.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: './dist/index.mjs',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    external: ['aptos', '@manahippo/move-to-ts', 'big-integer'],
+    plugins: [typescript(),json()]
+  }
+];
+export { rollup_config as default };
