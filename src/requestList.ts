37f3fa835ebfc94798aaf428f6afb56598ef4334
Vasilisa Versus <dcversus@gmail.com>
2023-08-10 13:01:05 +0100

    add WBTC layerzero (#99)

--- src/requestList.ts
+++ src/requestList.ts
@@ -1361,4 +1361,25 @@ export const REQUESTS: RawCoinInfo[] = [
     source: "native",
     permissioned_listing: false,
   },
+  {
+    name: "Wrapped BTC (LayerZero)",
+    symbol: "zWBTC",
+    official_symbol: "WBTC",
+    coingecko_id: "wbtc",
+    decimals: 6,
+    logo_url: "https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/BTC.webp",
+    project_url: "",
+    token_type: {
+      type: "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::WBTC",
+      account_address: "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa",
+      module_name: "asset",
+      struct_name: "WBTC"
+    },
+    extensions: {
+      data: [["bridge", "layerzero"]]
+    },
+    unique_index: 3005,
+    source: "layerzero",
+    permissioned_listing: true,
+  },
 ];
